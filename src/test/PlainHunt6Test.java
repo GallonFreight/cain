4746b2e712fca03ccf457eac0cddceb4f531f6d9
GallonFreight <gallonfreight+accounts@proton.me>
2025-07-07 17:27:26 +0100

    write two more tests of elementary composition

--- src/test/PlainHunt6Test.java
+++ src/test/PlainHunt6Test.java
@@ -2,15 +2,38 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import logic.PlainHunt6;
+import logic.*;
 
 class PlainHunt6Test {
 
     @Test
-    void testFirstRow() {
+    void testLeadHead() {
         assertArrayEquals(
                           new int[] {1, 2, 3, 4, 5, 6},
                           new PlainHunt6().currentRow()
+                          );
+    }
+
+    @Test
+    void testFirstChange() {
+        Composition ph = new PlainHunt6();
+        ph.change();
+
+        assertArrayEquals(
+                          new int[] {2, 1, 4, 3, 6, 5},
+                          ph.currentRow()
+                          );
+    }
+
+    @Test
+    void testLeadEnd() {
+        Composition ph = new PlainHunt6();
+        for (int i = 0; i < 11; i++)
+            ph.change();
+
+        assertArrayEquals(
+                          new int[] {1, 3, 2, 5, 4, 6},
+                          ph.currentRow()
                           );
     }
 }
